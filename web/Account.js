dd73e34139aebbb703a4cf81efae0f8b9c1e0f95
PawanKalla <farmfriend.dev@gmail.com>
2021-03-17 12:13:50 +0530

    Pawan commit

--- web/Account.js
+++ web/Account.js
@@ -1,11 +1,43 @@
+
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     var user = firebase.auth().currentUser;
 
     if(user != null){
+      const storageRef = firebase.storage().ref();
+    var user = firebase.auth().currentUser;
+    var user_id = user.uid;
+    		// [START storage_download_via_url]
+        const storageRef1 = firebase.storage().ref();
 
-      var email_id = user.displayName;
-      document.getElementById("user_para").innerHTML = "Name : " + email_id;
+        		// [START storage_download_via_url]
+        		storageRef1.child(user_id+'.jpeg').getDownloadURL()
+        			.then((url) => {
+
+        				// This can be downloaded directly:
+        				var xhr = new XMLHttpRequest();
+        				xhr.responseType = 'blob';
+        				xhr.onload = (event) => {
+        					var blob = xhr.response;
+        				};
+        				xhr.open('GET', url);
+        				xhr.send();
+
+        				// Or inserted into an <img> element
+        				var img = document.getElementById('myImg');
+        				img.setAttribute('src', url);
+        			})
+        			.catch((error) => {
+        				window.alert('Upload Photo');
+        			});
+          var fileButton = document.getElementById('photo');
+      fileButton.addEventListener('change', function(e){
+     	 var file = e.target.files[0];
+     	 var storageRef = firebase.storage().ref(user_id+'.jpeg');
+     	 var task = storageRef.put(file);
+       
+      });
+
     }
 
   } else {
@@ -13,8 +45,3 @@ firebase.auth().onAuthStateChanged(function(user) {
   window.location.replace("index.html");
   }
 });
-
-
-function update(){
-  window.alert("Ho gaya Bhai");
-}
